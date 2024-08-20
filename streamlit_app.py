e3ca4611835086b2c8751d52f52a24c837c9af63
ShriyanGanesh <97284192+ShriyanGanesh@users.noreply.github.com>
2024-08-20 12:08:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 
-# Show title and description.
-st.title("💬 TalkingBuddy")
-st.title("## Hello there")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
-import streamlit as st
+
 
 # Sample data for fish inventory
 fish_inventory = {
